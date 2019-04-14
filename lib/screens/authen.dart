bba15ffc8c871851a8d6265a99ae40299a22d3a0
note <notesoiton@gmail.com>
2019-04-14 14:49:22 +0700

    chage bg color

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,19 +6,39 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  // Show Logo
+  Widget showLogo() {
+    return Image.asset('images/logo.png');
+  }
+
   // App Name
   Widget showAppName() {
-    return Text('Note Restaurant');
+    return Text(
+      'นวพร ค้าข้าว',
+      style: TextStyle(
+          fontSize: 40.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.orangeAccent[700]),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.yellow[800], Colors.yellow[100]],
+                  begin: Alignment(-1, -1))),
           padding: EdgeInsets.only(top: 30.0),
           alignment: Alignment.topCenter,
           child: Column(
-            children: <Widget>[showAppName(), showAppName(), showAppName()],
+            children: <Widget>[
+              showLogo(),
+              Container(margin: EdgeInsets.only(top: 10.0),
+                child: showAppName(),
+              )
+            ],
           )),
     );
   }
