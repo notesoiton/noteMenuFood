33a30600cb8eed30fac0c78a80592a0826655c69
note <notesoiton@gmail.com>
2019-04-21 01:23:54 +0700

    4 section listView

--- lib/screens/menuHorizontalScroll.dart
+++ lib/screens/menuHorizontalScroll.dart
@@ -35,7 +35,39 @@ class ShowMenu extends StatelessWidget {
             style: TextStyle(color: Colors.white),
           ),
         ),
-        body: Container(
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+          Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 260,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              myArticles("http://funmusic.name/menu/menu01.jpg",
+                  "ไข่ปลาเรียวเซียว", "300บาท"),
+              myArticles("http://funmusic.name/menu/menu02.jpg", "ส้มตำปูม้า",
+                  "180บาท"),
+              myArticles("http://funmusic.name/menu/menu03.jpg",
+                  "แกงส้มไข่ปลาเรียวเซียว", "380บาท"),
+              myArticles("http://funmusic.name/menu/menu04.jpg", "แกงส้มกุ้งสด",
+                  "280บาท"),
+              myArticles("http://funmusic.name/menu/menu05.jpg", "หอยแครงลวก",
+                  "120บาท"),
+              myArticles("http://funmusic.name/menu/menu06.jpg", "แกงไหลบัว",
+                  "180บาท"),
+              myArticles("http://funmusic.name/menu/menu07.jpg",
+                  "ตำซั่วหอยแครง", "140บาท"),
+              myArticles("http://funmusic.name/menu/menu08.jpg", "คอหมูย่าง",
+                  "100บาท"),
+              myArticles("http://funmusic.name/menu/menu09.jpg",
+                  "ต้มแซ่บซุปเปอร์", "270บาท"),
+              myArticles("http://funmusic.name/menu/menu10.jpg",
+                  "สามชั้นทอดน้ำปลา", "140บาท"),
+            ],
+          ),
+        ),//First
+        Container(
           margin: EdgeInsets.symmetric(vertical: 20.0),
           height: 260,
           child: ListView(
@@ -63,7 +95,66 @@ class ShowMenu extends StatelessWidget {
                   "สามชั้นทอดน้ำปลา", "140บาท"),
             ],
           ),
-        ),
+        ),//second
+Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 260,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              myArticles("http://funmusic.name/menu/menu01.jpg",
+                  "ไข่ปลาเรียวเซียว", "300บาท"),
+              myArticles("http://funmusic.name/menu/menu02.jpg", "ส้มตำปูม้า",
+                  "180บาท"),
+              myArticles("http://funmusic.name/menu/menu03.jpg",
+                  "แกงส้มไข่ปลาเรียวเซียว", "380บาท"),
+              myArticles("http://funmusic.name/menu/menu04.jpg", "แกงส้มกุ้งสด",
+                  "280บาท"),
+              myArticles("http://funmusic.name/menu/menu05.jpg", "หอยแครงลวก",
+                  "120บาท"),
+              myArticles("http://funmusic.name/menu/menu06.jpg", "แกงไหลบัว",
+                  "180บาท"),
+              myArticles("http://funmusic.name/menu/menu07.jpg",
+                  "ตำซั่วหอยแครง", "140บาท"),
+              myArticles("http://funmusic.name/menu/menu08.jpg", "คอหมูย่าง",
+                  "100บาท"),
+              myArticles("http://funmusic.name/menu/menu09.jpg",
+                  "ต้มแซ่บซุปเปอร์", "270บาท"),
+              myArticles("http://funmusic.name/menu/menu10.jpg",
+                  "สามชั้นทอดน้ำปลา", "140บาท"),
+            ],
+          ),
+        ),//thrid
+Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 260,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              myArticles("http://funmusic.name/menu/menu01.jpg",
+                  "ไข่ปลาเรียวเซียว", "300บาท"),
+              myArticles("http://funmusic.name/menu/menu02.jpg", "ส้มตำปูม้า",
+                  "180บาท"),
+              myArticles("http://funmusic.name/menu/menu03.jpg",
+                  "แกงส้มไข่ปลาเรียวเซียว", "380บาท"),
+              myArticles("http://funmusic.name/menu/menu04.jpg", "แกงส้มกุ้งสด",
+                  "280บาท"),
+              myArticles("http://funmusic.name/menu/menu05.jpg", "หอยแครงลวก",
+                  "120บาท"),
+              myArticles("http://funmusic.name/menu/menu06.jpg", "แกงไหลบัว",
+                  "180บาท"),
+              myArticles("http://funmusic.name/menu/menu07.jpg",
+                  "ตำซั่วหอยแครง", "140บาท"),
+              myArticles("http://funmusic.name/menu/menu08.jpg", "คอหมูย่าง",
+                  "100บาท"),
+              myArticles("http://funmusic.name/menu/menu09.jpg",
+                  "ต้มแซ่บซุปเปอร์", "270บาท"),
+              myArticles("http://funmusic.name/menu/menu10.jpg",
+                  "สามชั้นทอดน้ำปลา", "140บาท"),
+            ],
+          ),
+        ),//fourth
+        ],)
       ),
     );
   }
